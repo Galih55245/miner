eba970054d5d1878007dddd0ff6285b083f1ee33
Galih55245 <87139035+Galih55245@users.noreply.github.com>
2021-07-08 22:37:40 +0700

    Create monerocoin.sh

---
+++ monerocoin.sh
@@ -0,0 +1,6 @@
+# monerocoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz 
+tar -xf xmrig-6.12.1-linux-x64.tar.gz
+cd xmrig-6.12.1
+./xmrig -o pool.minexmr.com:4444 -u 83cuUrf33prSfctbrsTqu4E4LkV3g22HuGESDxgRTTcJ6H2TrAJ2remdauWPQ4cd36cMn9UPfvdd1NEXDYkpuYNjDrYfSdC.gpp -p x --cpu 6
